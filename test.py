97680f1201e2c6d526117be6647bdbbe38d0f4a5
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-10-03 19:29:52 +0300

    More examples

--- test.py
+++ test.py
@@ -8,18 +8,18 @@ m1 = MeshTri.init_tensor(np.linspace(0, 1, 3),
                          np.linspace(0, 1, 4))
 m2 = MeshTri.init_sqsymmetric().refined(1)
 
-m1 = MeshQuad.init_tensor(np.linspace(0, 1, 3),
-                          np.linspace(0, 1, 4))
-m2 = MeshQuad().refined(2)
-
 # m1 = MeshQuad.init_tensor(np.linspace(0, 1, 3),
 #                           np.linspace(0, 1, 4))
-# m2 = MeshTri().refined(2)
+# m2 = MeshQuad().refined(2)
 
-# m1 = MeshTet()
-# m2 = MeshTet.init_tensor(np.linspace(0, 1, 2),
-#                          np.linspace(0, 1, 2),
-#                          np.linspace(0, 1, 3))
+m1 = MeshQuad.init_tensor(np.linspace(0, 1, 3),
+                          np.linspace(0, 1, 4))
+m2 = MeshTri().refined(2)
+
+m1 = MeshTet()
+m2 = MeshTet.init_tensor(np.linspace(0, 1, 2),
+                         np.linspace(0, 1, 2),
+                         np.linspace(0, 1, 3))
 
 p1 = np.asfortranarray(m1.p)
 t1 = np.asfortranarray(m1.t + 1)
@@ -58,7 +58,11 @@ elif dim == 3:
 
 print(out)
 
-m1.draw(color='red')
-m2.draw(color='blue')
-m12.draw(color='black').show()
+if dim == 2:
+    axs = plt.subplots(1, 3)
+elif dim == 3:
+    axs = plt.subplots(1, 3, subplot_kw=dict(projection='3d'))
+m1.draw(ax=axs[1][0], color='red')
+m2.draw(ax=axs[1][1], color='blue')
+m12.draw(ax=axs[1][2], color='black').show()
 #ax.plot(out[0], out[1], 'bo')
